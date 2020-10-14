e1e6d84ced2d23cdd1a28085e674d5773bf8a70e
Frederick Javalera <frederickjavalera@gmail.com>
2020-10-13 21:50:19 -0600

    Copied starting logic in ExampleWithoutUsingDelegates.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace ExampleUsingDelegates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculate();
+        }
+
+        private static void Calculate()
+        {
+            var left = int.Parse(Console.ReadLine());
+            var right = int.Parse(Console.ReadLine());
+
+            var sum = Sum(left, right);
+
+            Print(sum);
+        }
+
+        private static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static int Product(int a, int b)
+        {
+            return a * b;
+        }
+
+        private static void Print(int result)
+        {
+            System.Console.WriteLine($"Result is {result}.");
         }
     }
 }
