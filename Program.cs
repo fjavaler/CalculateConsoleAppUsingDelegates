8a7d37c80fbc18e5a84b1c414de77d0580f27b38
Frederick Javalera <frederickjavalera@gmail.com>
2020-10-13 22:15:45 -0600

    Added message prompts.

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ namespace ExampleUsingDelegates
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine(">Starting.");
             Calculate();
+            System.Console.WriteLine(">Ended...");
         }
 
         private static void Calculate()
         {
+            System.Console.WriteLine("Please enter a number:");
             var left = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Please enter another number:");
             var right = int.Parse(Console.ReadLine());
 
             var sum = Sum(left, right);
